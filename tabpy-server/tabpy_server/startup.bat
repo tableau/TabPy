6f1fc3fb9c73ecd4fdaf0b5d82ab27772f795656
BBeran <bora.beran@gmail.com>
2016-10-28 01:27:12 -0700

    Use batch file location "%~dp0" instead of %CD%

--- tabpy-server/tabpy_server/startup.bat
+++ tabpy-server/tabpy_server/startup.bat
@@ -4,20 +4,13 @@ set port=9004
 set port=%1
 )
 @SET CONDA_ENVIRONMENT=Tableau-Python-Server
-@SET TABPY_STATE_PATH=%CD%
+@SET SETUP_PATH="%~dp0"
 @CD ..\..\..
 @CD Scripts
-@call activate %CONDA_ENVIRONMENT%
-
-@IF NOT EXIST %TABPY_STATE_PATH%\state.ini (
-@copy %TABPY_STATE_PATH%\state.ini.template %TABPY_STATE_PATH%\state.ini
+@CALL activate %CONDA_ENVIRONMENT%
+@CD %SETUP_PATH%
+@IF NOT EXIST state.ini (
+@copy state.ini.template state.ini
 )
-
-@set PYTHONPATH=%PYTHONPATH%;%TABPY_STATE_PATH%
-python %TABPY_STATE_PATH%/tabpy.py --port %port%
-
-
-
-
-
-
+@set PYTHONPATH=%PYTHONPATH%;%SETUP_PATH%
+python tabpy.py --port %port%
