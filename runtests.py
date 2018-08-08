989cac5e934bea4ef3047ab344696305a73e6256
Will Ayd <william.ayd@icloud.com>
2018-08-08 14:02:28 -0700

    Absolute imports and refactor of runtests

--- runtests.py
+++ runtests.py
@@ -4,13 +4,16 @@ import unittest
 
 
 if __name__ == '__main__':
-    sys.path.insert(0, os.path.dirname(__file__).join('tabpy-client'))
-    sys.path.insert(0, os.path.dirname(__file__).join('tabpy-server'))
+    dirs = {'tabpy-client', 'tabpy-server'}
+
+    for dir_ in dirs:
+        sys.path.insert(0, os.path.join(
+            os.path.abspath(os.path.dirname(__file__)), dir_))
 
     # Get all of the tests we need from the two project
     suite_list = []
-    for dir in ('tabpy-client', 'tabpy-server'):
-        suite_list.append(unittest.TestLoader().discover(dir))
+    for dir_ in ('tabpy-client', 'tabpy-server'):
+        suite_list.append(unittest.TestLoader().discover(dir_))
 
     suite = unittest.TestSuite(suite_list)
 
