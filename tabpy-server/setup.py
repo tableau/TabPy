95172737a87f793e2cf44a39be20615d47ae8496
nmannheimer <nathanmannheimer@gmail.com>
2019-01-27 19:19:07 -0800

    Update setup.py

--- tabpy-server/setup.py
+++ tabpy-server/setup.py
@@ -8,7 +8,7 @@ except ImportError as err:
 
 setup(
     name='tabpy-server',
-    python-requires='>=3.7.2',
+    python_requires='>=3.7.2',
     version=versioneer.get_version(),
     description='Web server Tableau uses to run Python scripts.',
     url='https://github.com/tableau/TabPy',
