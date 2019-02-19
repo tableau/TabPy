443dcaeddc3be2d34b61ac25cf812d56a96f174f
Jessica Negara <jessica.negara@gmail.com>
2019-02-19 11:44:36 -0800

    Implemented code review feedback.

--- tabpy-server/server_tests/test_service_info_handler.py
+++ tabpy-server/server_tests/test_service_info_handler.py
@@ -1,82 +1,93 @@
 from tornado.testing import AsyncHTTPTestCase
 from tabpy_server.app import TabPyApp
 import simplejson as json
-from tabpy_server import __version__
 import tempfile, os
 
 
 def _create_expected_info_response(settings, tabpy_state):
-    response = {}
-    response['description'] = tabpy_state.get_description()
-    response['creation_time'] = tabpy_state.creation_time
-    response['state_path'] = settings['state_file_path']
-    response['server_version'] = settings['server_version']
-    response['name'] = tabpy_state.name
-    response['versions'] = settings['versions']
-    return response
-
-
-def _get_app_and_state(config_file=None):
-    tabpy_app = TabPyApp(config_file=config_file)
-    tornado_app = tabpy_app._create_tornado_web_app()
-
-    return tornado_app, tabpy_app.settings, tabpy_app.tabpy_state
+    return {
+        'description': tabpy_state.get_description(),
+        'creation_time': tabpy_state.creation_time,
+        'state_path': settings['state_file_path'],
+        'server_version': settings['server_version'],
+        'name': tabpy_state.name,
+        'versions': settings['versions']
+    }
 
 
 class TestServiceInfoHandlerDefault(AsyncHTTPTestCase):
     def get_app(self):
-        app, self.settings, self.state = _get_app_and_state()
-        return app
+        self.app = TabPyApp()
+        return self.app._create_tornado_web_app()
 
-    def test_info(self):
-        """
-        Tests that /info returns the correct response on a vanilla TabPy instance.
-        """
+    def test_given_vanilla_tabpy_server_expect_correct_info_response(self):
         response = self.fetch('/info')
         self.assertEqual(response.code, 200)
         actual_response = json.loads(response.body)
-        expected_response = _create_expected_info_response(self.settings, self.state)
+        expected_response = _create_expected_info_response(self.app.settings, self.app.tabpy_state)
 
         self.assertDictEqual(actual_response, expected_response)
 
 
 class TestServiceInfoHandlerWithAuth(AsyncHTTPTestCase):
+    @classmethod
+    def setUpClass(cls):
+        prefix = '__TestServiceInfoHandlerWithAuth_'
+        # create password file
+        cls.pwd_file = tempfile.NamedTemporaryFile(prefix=prefix, suffix='.txt', delete=False)
+        cls.pwd_file.write(b'username password')
+        cls.pwd_file.close()
+
+        # create state.ini dir and file
+        cls.state_dir = tempfile.mkdtemp(prefix=prefix)
+        cls.state_file = open(os.path.join(cls.state_dir, 'state.ini'), 'w+')
+        cls.state_file.write('[Service Info]\n'
+                             'Name = TabPy Serve\n'
+                             'Description = \n'
+                             'Creation Time = 0\n'
+                             'Access-Control-Allow-Origin = \n'
+                             'Access-Control-Allow-Headers = \n'
+                             'Access-Control-Allow-Methods = \n'
+                             '\n'
+                             '[Query Objects Service Versions]\n'
+                             '\n'
+                             '[Query Objects Docstrings]\n'
+                             '\n'
+                             '[Meta]\n'
+                             'Revision Number = 1\n')
+        cls.state_file.close()
+
+        # create config file
+        cls.config_file = tempfile.NamedTemporaryFile(prefix=prefix, suffix='.conf', delete=False)
+        cls.config_file.write(bytes('[TabPy]\n'
+                        'TABPY_PWD_FILE = {}\n'
+                        'TABPY_STATE_PATH = {}'.format(cls.pwd_file.name, cls.state_dir), 'utf-8'))
+        cls.config_file.close()
+
+    @classmethod
+    def tearDownClass(cls):
+        os.remove(cls.pwd_file.name)
+        os.remove(cls.state_file.name)
+        os.remove(cls.config_file.name)
+        os.rmdir(cls.state_dir)
+
     def get_app(self):
-        test_pwd_file = tempfile.NamedTemporaryFile(prefix='___TestServiceInfoHandlerWithAuth_', suffix='.txt', delete=False)
-        test_pwd_file.write(b'username password')
-        test_pwd_file.close()
+        self.app = TabPyApp(self.config_file.name)
+        return self.app._create_tornado_web_app()
 
-        test_config = tempfile.NamedTemporaryFile(prefix='___TestServiceInfoHandlerWithAuth_', suffix='.conf',
-                                                  delete=False)
-        config = '[TabPy]\n' \
-                 'TABPY_PWD_FILE = {}\n' \
-                 'TABPY_STATE_PATH = ./tabpy-server/tabpy_server'.format(test_pwd_file.name)
-        test_config.write(bytes(config, 'utf-8'))
-        test_config.close()
-
-        app, self.settings, self.state = _get_app_and_state(test_config.name)
-
-        os.remove(test_config.name)
-        os.remove(test_pwd_file.name)
-
-        return app
-
-    def test_info(self):
-        """
-        Tests that /info returns thecorrect response on a TabPy instance with auth enabled.
-        """
+    def test_given_tabpy_server_with_auth_expect_correct_info_response(self):
         response = self.fetch('/info')
         self.assertEqual(response.code, 200)
         actual_response = json.loads(response.body)
-        expected_response = _create_expected_info_response(self.settings, self.state)
+        expected_response = _create_expected_info_response(self.app.settings, self.app.tabpy_state)
 
         self.assertDictEqual(actual_response, expected_response)
         self.assertTrue('versions' in actual_response)
         versions = actual_response['versions']
-        self.assertTrue('v0.4' in versions)
-        v0_4 = versions['v0.4']
-        self.assertTrue('features' in v0_4)
-        features = v0_4['features']
+        self.assertTrue('v1' in versions)
+        v1 = versions['v1']
+        self.assertTrue('features' in v1)
+        features = v1['features']
         self.assertDictEqual({
             'authentication': {
                 'methods': {
