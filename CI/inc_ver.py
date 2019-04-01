0668a0e5482a90229be839c9ea6ca9f821728949
ogolovatyi <ogolovatyi@OGOLOVATYI.tsi.lan>
2019-04-01 13:30:18 -0700

    Add CI version update script

---
+++ CI/inc_ver.py
@@ -0,0 +1,38 @@
+'''
+Update version (VERSION file in the repository root)
+with commit number:
+major.minor.commit
+
+The script is only to be used in Travis CI,
+see .travis.yml for more details.
+
+The script uses Travis enviroment variables, see
+https://docs.travis-ci.com/user/environment-variables/#default-environment-variables
+for more details.
+'''
+
+import os
+
+
+def main():
+    version = '0.0.0'
+    with open('VERSION') as f:
+        version = f.read()
+    ver = version.split('.')
+    commit = os.environ.get('TRAVIS_COMMIT')
+    new_ver = f'{ver[0]}.{ver[1]}.{commit}'
+
+    with open('VERSION', 'w') as f:
+        f.write(new_ver)
+
+    os.system('git add -u')
+    os.system(f'git commit -m "[ci skip] increase version to {new_version}')
+    os.system('git push')
+
+
+if __name__ == "__main__":
+    if not os.environ.get('TRAVIS', False):
+        print(__doc__)
+        return
+
+    main()
