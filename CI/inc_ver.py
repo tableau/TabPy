c95455b441e87c50cbbe481a1e6ba61c01c52138
Travis CI <travis@travis-ci.org>
2019-04-03 15:03:20 -0700

    [ci skip] increase version to 0.4.None

--- CI/inc_ver.py
+++ CI/inc_ver.py
@@ -15,9 +15,11 @@ import os
 
 
 def main():
+    '''
     if not os.environ.get('TRAVIS', False):
         print(__doc__)
         return 1
+    '''
 
     version = '0.0.0'
     with open('VERSION') as f:
@@ -25,10 +27,14 @@ def main():
     ver = version.split('.')
     commit = os.environ.get('TRAVIS_COMMIT')
     new_ver = f'{ver[0]}.{ver[1]}.{commit}'
+    gh_token = os.environ.get('GH_TOKEN')
 
     with open('VERSION', 'w') as f:
         f.write(new_ver)
 
+    os.system(f'git config --global user.email "travis@travis-ci.org"')
+    os.system(f'git config --global user.name "Travis CI"')
+    os.system(f'git remote set-url origin https://{gh_token}@github.com/tableau/TabPy > /dev/null 2>&1')
     os.system(f'git add -u')
     os.system(f'git commit -m "[ci skip] increase version to {new_ver}"')
     branch = os.environ.get('TRAVIS_BRANCH')
