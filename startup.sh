483fe24941ef06521c407c5a1600cb498da9cb57
John <johng42@users.noreply.github.com>
2019-04-23 14:50:23 -0700

    Merge pull request #268 from tableau/print-install-logs
    Build is broken.

--- startup.sh
+++ startup.sh
@@ -7,6 +7,24 @@ function check_status() {
     fi
 }
 
+function install_dependencies() {
+    # $1 = tabpy_server | tabpy_tools
+    # $2 = true if install logs are printed to the console,
+    #      false if they are printed to a log file
+    # $3 = install log file path
+    if [ "$2" = true ]; then
+        echo -e "\nInstalling ${1} dependencies..."
+        python3 setup.py install
+    elif [ "$2" = false ]; then
+        echo -e "\nInstalling ${1} dependencies..." >> ${3}
+        python3 setup.py install >> ${3} 2>&1
+    else
+        echo Invalid startup environment.
+        exit 1
+    fi
+    check_status
+}
+
 # Check for Python in PATH
 echo Checking for presence of Python in the system path variable.
 python --version &>-
@@ -16,47 +34,51 @@ check_status "Cannot find Python.  Check that Python is installed and is in the 
 echo Setting TABPY_ROOT to current working directory.
 TABPY_ROOT=$PWD
 INSTALL_LOG=${TABPY_ROOT}/tabpy-server/install.log
+echo "" > ${INSTALL_LOG}
+PRINT_INSTALL_LOGS=false
+
+# Parse CLI parameters
+for i in "$@"
+do
+    case $i in
+        -c=*|--config=*)
+        CONFIG="${i#*=}"
+        shift
+        ;;
+        --no-startup)
+        NO_STARTUP=true
+        shift
+        ;;
+        --print-install-logs)
+        PRINT_INSTALL_LOGS=true
+        shift
+        ;;
+        *)
+        echo Invalid option: $i
+    esac
+done
 
 # Check for dependencies, install them if they're not present.
 echo Installing TabPy-server requirements.
-echo Read the logs at ${INSTALL_LOG}
+if [ "$PRINT_INSTALL_LOGS" = false ]; then
+    echo Read the logs at ${INSTALL_LOG}
+fi
 
 cd ${TABPY_ROOT}/tabpy-server
-echo -e "\nInstalling tabpy-server dependencies..." > ${INSTALL_LOG}
-python3 setup.py install >> ${INSTALL_LOG} 2>&1
-check_status
+install_dependencies "tabpy-server" ${PRINT_INSTALL_LOGS} ${INSTALL_LOG}
 
 cd ${TABPY_ROOT}/tabpy-tools
-echo -e "\nInstalling tabpy-tools dependencies..." >> ${INSTALL_LOG}
-python3 setup.py install >> ${INSTALL_LOG} 2>&1
-check_status
+install_dependencies "tabpy-tools" ${PRINT_INSTALL_LOGS} ${INSTALL_LOG}
 
 cd ${TABPY_ROOT}
 check_status
-
-# Check for CLI parameters
-for i in "$@"
-do
-case $i in
-	-c=*|--config=*)
-	CONFIG="${i#*=}"
-	shift
-	;;
-	--no-startup)
-	TEST_ENV=true
-	shift
-	;;
-	*)
-	echo Invalid option: $i
-esac
-done
 
 if [ ! -z ${CONFIG} ]; then
     echo Using the config file at ${TABPY_ROOT}/tabpy-server/$CONFIG.
 fi
 
 # Exit if in a test environent
-if [ ! -z ${TEST_ENV} ]; then
+if [ ! -z ${NO_STARTUP} ]; then
     echo Skipping server startup. Exiting successfully.
 	exit 0
 fi
