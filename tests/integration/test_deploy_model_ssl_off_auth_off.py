259d4c29685cbbf5adbd9f77f819d9702824bab0
Sasha Babayan <sbabayan@tableau.com>
2019-05-07 12:15:47 -0700

    added integration tests and addressed feedback

---
+++ tests/integration/test_deploy_model_ssl_off_auth_off.py
@@ -0,0 +1,20 @@
+import integ_test_base
+import subprocess
+from pathlib import Path
+
+class TestDeployModelSSLOffAuthOff(integ_test_base.IntegTestBase):
+    def test_deploy_ssl_off_auth_off(self):
+        path = str(Path('models', 'setup.py'))
+        subprocess.call([self.py, path, self._get_config_file_name()])
+        
+        conn = self._get_connection()
+        conn.request("GET", "/endpoints/PCA")
+        PCA_request = conn.getresponse()
+        self.assertEqual(200, PCA_request.status)
+        PCA_request.read()
+
+        conn.request("GET", "/endpoints/Sentiment%20Analysis")
+        SentimentAnalysis_request = conn.getresponse()
+        self.assertEqual(200, SentimentAnalysis_request.status)
+        SentimentAnalysis_request.read()
+        
---
+++ tests/integration/test_deploy_model_ssl_off_auth_on.py
@@ -0,0 +1,33 @@
+import integ_test_base
+import base64
+import requests
+import subprocess
+from pathlib import Path
+
+class TestDeployModelSSLOffAuthOn(integ_test_base.IntegTestBase):
+    def _get_pwd_file(self) -> str:
+        return './tests/integration/resources/pwdfile.txt'
+
+    def test_deploy_ssl_off_auth_on_valid_credentials(self):
+        path = str(Path('models', 'setup.py'))
+        p = subprocess.run([self.py, path, self._get_config_file_name()],
+                           input=b'user1\nP@ssw0rd\n')
+
+        headers = {
+            'Content-Type': "application/json",
+            'TabPy-Client': "Integration test for deploying models with auth",
+            'Authorization':
+                'Basic ' +
+                base64.b64encode('user1:P@ssw0rd'.encode('utf-8')).decode('utf-8')
+            }
+        
+        conn = self._get_connection()
+        conn.request("GET", "/endpoints/PCA", headers=headers)
+        PCA_request = conn.getresponse()
+        self.assertEqual(200, PCA_request.status)
+        PCA_request.read()
+
+        conn.request("GET", "/endpoints/Sentiment%20Analysis", headers=headers)
+        SentimentAnalysis_request = conn.getresponse()
+        self.assertEqual(200, SentimentAnalysis_request.status)
+        SentimentAnalysis_request.read()
