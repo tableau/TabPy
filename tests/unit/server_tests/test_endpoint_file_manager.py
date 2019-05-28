aa91f6855da5057b1e06ddeb145e25db21aff379
Sasha Babayan <sbabayan@tableau.com>
2019-05-28 13:54:30 -0700

    simple unit tests to increase code coverage, removed unused imports from other unit tests

---
+++ tests/unit/server_tests/test_endpoint_file_manager.py
@@ -0,0 +1,13 @@
+import unittest
+from tabpy_server.common.endpoint_file_mgr import _check_endpoint_name
+
+
+class TestEndpointFileManager(unittest.TestCase):
+    def test_endpoint_name_not_str(self):
+        self.assertRaises(TypeError, _check_endpoint_name, 2)
+
+    def test_endpoint_name_empty_str(self):
+        self.assertRaises(ValueError, _check_endpoint_name, "")
+
+    def test_endpoint_name_wrong_regex(self):
+        self.assertRaises(ValueError, _check_endpoint_name, "****")
