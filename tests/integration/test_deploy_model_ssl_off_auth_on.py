65ed537c24e0839070aa5abea1077865be446cda
sbabayan <34922408+sbabayan@users.noreply.github.com>
2019-06-07 09:41:53 -0700

    adding t-test to pre-deployed models (#312)
    * adding t-test to pre-deployed models
    
    * fixed pep8 issues
    
    * import error on ttest and updated successful deployment message for sentiment analysis
    
    * fixed md issues
    
    * fixed endpoint name in integration tests
    
    * refactor redundant code and update md

--- tests/integration/test_deploy_model_ssl_off_auth_on.py
+++ tests/integration/test_deploy_model_ssl_off_auth_on.py
@@ -9,6 +9,7 @@ class TestDeployModelSSLOffAuthOn(integ_test_base.IntegTestBase):
         return './tests/integration/resources/pwdfile.txt'
 
     def test_deploy_ssl_off_auth_on(self):
+        models = ['PCA', 'Sentiment%20Analysis', "ttest"]
         path = str(Path('models', 'setup.py'))
         p = subprocess.run([self.py, path, self._get_config_file_name()],
                            input=b'user1\nP@ssw0rd\n')
@@ -20,15 +21,11 @@ class TestDeployModelSSLOffAuthOn(integ_test_base.IntegTestBase):
                 'Basic ' +
                 base64.b64encode('user1:P@ssw0rd'.
                                  encode('utf-8')).decode('utf-8')
-            }
+        }
 
         conn = self._get_connection()
-        conn.request("GET", "/endpoints/PCA", headers=headers)
-        PCA_request = conn.getresponse()
-        self.assertEqual(200, PCA_request.status)
-        PCA_request.read()
-
-        conn.request("GET", "/endpoints/Sentiment%20Analysis", headers=headers)
-        SentimentAnalysis_request = conn.getresponse()
-        self.assertEqual(200, SentimentAnalysis_request.status)
-        SentimentAnalysis_request.read()
+        for m in models:
+            conn.request("GET", f'/endpoints/{m}', headers=headers)
+            m_request = conn.getresponse()
+            self.assertEqual(200, m_request.status)
+            m_request.read()
