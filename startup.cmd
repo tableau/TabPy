ae8ea194ef4143b571f790dc6aec9ab6fab901c1
ogolovatyi <ogolovatyi@OGOLOVATYI.tsi.lan>
2019-08-08 14:58:23 -0700

    Change startup scripts to tell about pip tabpy

---
+++ startup.cmd
@@ -0,0 +1,3 @@
+@ECHO off
+ECHO TabPy is a PIP package now, install it with "pip install tabpy".
+ECHO For more information read https://tableau.github.io/TabPy/.
---
+++ startup.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo TabPy is a PIP package now, install it with "pip install tabpy".
+echo For more information read https://tableau.github.io/TabPy/.
