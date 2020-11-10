5df0e00cde7584681abb9ce1dba7c5bbaafc4c81
Oleksandr Golovatyi <olekg@outlook.com>
2020-11-10 10:28:04 -0800

    Hotfix: model redeployment (#462)
    * Fix model redeployment
    
    * Fix style and update version
    
    Co-authored-by: Olek Golovatyi <ogolovatyi@salesforce.com>

--- tests/integration/test_deploy_model_ssl_on_auth_on.py
+++ tests/integration/test_deploy_model_ssl_on_auth_on.py
@@ -45,3 +45,36 @@ class TestDeployModelSSLOnAuthOn(integ_test_base.IntegTestBase):
                 headers=headers,
             )
             self.assertEqual(200, m_response.status_code)
+
+    def test_override_model_ssl_on_auth_on(self):
+        # Uncomment the following line to preserve
+        # test case output and other files (config, state, ect.)
+        # in system temp folder.
+        # self.set_delete_temp_folder(False)
+
+        self.deploy_models(self._get_username(), self._get_password())
+
+        # Override models
+        self.deploy_models(self._get_username(), self._get_password())
+
+        headers = {
+            "Content-Type": "application/json",
+            "TabPy-Client": "Integration test for deploying models with auth",
+            "Authorization": "Basic "
+            + base64.b64encode("user1:P@ssw0rd".encode("utf-8")).decode("utf-8"),
+        }
+
+        session = requests.Session()
+        # Do not verify servers' cert to be signed by trusted CA
+        session.verify = False
+        # Do not warn about insecure request
+        requests.packages.urllib3.disable_warnings()
+
+        models = ["PCA", "Sentiment%20Analysis", "ttest", "anova"]
+        for m in models:
+            m_response = session.get(
+                url=f"{self._get_transfer_protocol()}://"
+                f"localhost:9004/endpoints/{m}",
+                headers=headers,
+            )
+            self.assertEqual(200, m_response.status_code)
