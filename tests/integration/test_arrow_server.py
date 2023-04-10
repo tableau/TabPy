e60082bea2730e620736a5f7b1779a8f10d2e82f
Andy Antes <aantes@salesforce.com>
2023-04-10 12:19:50 -0400

    Clean up test module

--- tests/integration/test_arrow_server.py
+++ tests/integration/test_arrow_server.py
@@ -24,32 +24,33 @@ class TestArrowServer(unittest.TestCase):
     @classmethod
     def tearDownClass(cls):
         cls.arrow_server.shutdown()
-
+    
     def setUp(self):
         self.resources_path = os.path.join(os.path.dirname(__file__), "resources")
         self.arrow_server.flights = {}
 
-    def test_server_do_put(self):
-        data_path = os.path.join(self.resources_path, "data.csv")
+    def get_descriptor(self, data_path):
+        return pyarrow.flight.FlightDescriptor.for_path(data_path)
+
+    def write_data(self, data_path):
         table = csv.read_csv(data_path)
-        descriptor = pyarrow.flight.FlightDescriptor.for_path(data_path)
+        descriptor = self.get_descriptor(data_path)
         writer, _ = self.arrow_client.do_put(descriptor, table.schema)
         writer.write_table(table)
         writer.close()
+        return table
+
+    def test_server_do_put(self):
+        self.write_data(os.path.join(self.resources_path, "data.csv"))
         flight_info = list(self.arrow_server.list_flights(None, None))
         self.assertEqual(len(flight_info), 1)
 
     def test_server_do_get(self):
-        data_path = os.path.join(self.resources_path, "data.csv")
-        table = csv.read_csv(data_path)
-        descriptor = pyarrow.flight.FlightDescriptor.for_path(data_path)
-        writer, _ = self.arrow_client.do_put(descriptor, table.schema)
-        writer.write_table(table)
-        writer.close()
+        table = self.write_data(os.path.join(self.resources_path, "data.csv"))
+        descriptor = self.get_descriptor(os.path.join(self.resources_path, "data.csv"))
         self.assertEqual(len(self.arrow_server.flights), 1)
         info = self.arrow_client.get_flight_info(descriptor)
-        endpoint = info.endpoints[0]
-        reader = self.arrow_client.do_get(endpoint.ticket)
+        reader = self.arrow_client.do_get(info.endpoints[0].ticket)
         self.assertTrue(reader.read_all().equals(table))
         self.assertEqual(len(self.arrow_server.flights), 0)
 
