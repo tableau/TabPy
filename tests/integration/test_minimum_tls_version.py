2434b099e9cf0ec56bef6d525fbb7973c19a5032
Jake Ichikawa <jake.ichikawa@tableau.com>
2024-06-19 16:22:26 -0700

    Consolidate integration tests.

--- tests/integration/test_minimum_tls_version.py
+++ tests/integration/test_minimum_tls_version.py
@@ -1,14 +1,12 @@
 from . import integ_test_base
 import os
-import requests
 
-
-class TestMinimumTLSVersionValid(integ_test_base.IntegTestBase):
+class TestMinimumTLSVersion(integ_test_base.IntegTestBase):
     def _get_log_contents(self):
         with open(self.log_file_path, 'r') as f:
             return f.read()
 
-    def _get_config_file_name(self) -> str:
+    def _get_config_file_name(self, tls_version: str) -> str:
         config_file = open(os.path.join(self.tmp_dir, "test.conf"), "w+")
         config_file.write(
             "[TabPy]\n"
@@ -16,7 +14,7 @@ class TestMinimumTLSVersionValid(integ_test_base.IntegTestBase):
             "TABPY_TRANSFER_PROTOCOL = https\n"
             "TABPY_CERTIFICATE_FILE = ./tests/integration/resources/2019_04_24_to_3018_08_25.crt\n"
             "TABPY_KEY_FILE = ./tests/integration/resources/2019_04_24_to_3018_08_25.key\n"
-            "TABPY_MINIMUM_TLS_VERSION = TLSv1_3"
+            f"TABPY_MINIMUM_TLS_VERSION = {tls_version}"
         )
         pwd_file = self._get_pwd_file()
         if pwd_file is not None:
@@ -27,6 +25,19 @@ class TestMinimumTLSVersionValid(integ_test_base.IntegTestBase):
         self.delete_config_file = True
         return config_file.name
 
-    def test_minimum_tls_version_valid(self):
+class TestMinimumTLSVersionValid(TestMinimumTLSVersion):
+    def _get_config_file_name(self) -> str:
+        return super()._get_config_file_name("TLSv1_3")
+
+    def test_minimum_tls_version(self):
         log_contents = self._get_log_contents()
         self.assertIn("Setting minimum TLS version to TLSv1_3", log_contents)
+
+class TestMinimumTLSVersionInvalid(TestMinimumTLSVersion):
+    def _get_config_file_name(self) -> str:
+        return super()._get_config_file_name("TLSv-1.3")
+
+    def test_minimum_tls_version(self):
+        log_contents = self._get_log_contents()
+        self.assertIn("Unrecognized value for TABPY_MINIMUM_TLS_VERSION", log_contents)
+        self.assertIn("Setting minimum TLS version to TLSv1_2", log_contents)
