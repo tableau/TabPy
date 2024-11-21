3dbbd83a010185b2bcf2aef8bc25755bf384d234
Jake Ichikawa <jake.ichikawa@salesforce.com>
2024-11-21 09:15:05 -0800

    Test docstring update.

--- tabpy/tabpy_tools/client.py
+++ tabpy/tabpy_tools/client.py
@@ -380,7 +380,6 @@ class Client:
             description = obj.__doc__.strip() or "" if isinstance(obj.__doc__, str) else ""
 
         endpoint_object = CustomQueryObject(query=obj, description=description,)
-        docstring = "-- no docstring found in query function --"
 
         return {
             "name": name,
@@ -392,7 +391,7 @@ class Client:
             "methods": endpoint_object.get_methods(),
             "required_files": [],
             "required_packages": [],
-            "docstring": docstring,
+            "docstring": endpoint_object.get_doc_string(),
             "schema": copy.copy(schema),
             "is_public": is_public,
         }
